6e4987081cf3b1a1fa0bd7897c4437d4c7e8632a
Centny <centny@gmail.com>
2018-10-23 00:10:25 +0800

    update package script version.

--- bsrouter/gui/pkg.sh
+++ bsrouter/gui/pkg.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 rm -rf out
+pkg_ver=1.4.2
 npm run pack-$1
 cd out
 if [ "$1" == "osx" ];then
     plutil -insert LSUIElement -bool true BSRouter-darwin-x64/BSRouter.app/Contents/Info.plist
-    7za a -r BSRouter-darwin-x64.zip BSRouter-darwin-x64
+    mv BSRouter-darwin-x64 BSRouter-darwin-x64-$pkg_ver
+    7za a -r BSRouter-darwin-x64-$pkg_ver.zip BSRouter-darwin-x64-$pkg_ver
 fi
