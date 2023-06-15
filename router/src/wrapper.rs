649277351091b4656ff779c6688294c42ff5d224
Centny <centny@gmail.com>
2023-06-15 16:29:35 +0800

    add tun reader/writer.

--- router/src/wrapper.rs
+++ router/src/wrapper.rs
@@ -228,3 +228,102 @@ pub fn wrap_channel_w(buffer: usize) -> (Box<dyn frame::RawReader + Send + Sync>
     let txa = Box::new(WrapChannelWriter { inner: tx });
     (rxa, txa)
 }
+
+pub struct TunReader<T>
+where
+    T: AsyncRead + Send + Sync,
+{
+    inner: ReadHalf<T>,
+    #[cfg(any(target_os = "macos", target_os = "freebsd"))]
+    buffer: Vec<u8>,
+}
+
+impl<T> TunReader<T>
+where
+    T: AsyncRead + Send + Sync,
+{
+    pub fn new(inner: ReadHalf<T>) -> Self {
+        Self {
+            inner,
+            #[cfg(any(target_os = "macos", target_os = "freebsd"))]
+            buffer: vec![0u8; 2038],
+        }
+    }
+}
+
+#[async_trait]
+impl<T> frame::RawReader for TunReader<T>
+where
+    T: AsyncRead + Send + Sync,
+{
+    async fn read(&mut self, buf: &mut [u8]) -> tokio::io::Result<usize> {
+        #[cfg(any(target_os = "linux", target_os = "android"))]
+        {
+            let n = self.inner.read(buf).await?;
+            Ok(n)
+        }
+        #[cfg(any(target_os = "macos", target_os = "freebsd"))]
+        {
+            let n = self.inner.read(&mut self.buffer).await?;
+            buf[0..n - 4].copy_from_slice(&self.buffer[4..n]);
+            Ok(n)
+        }
+    }
+}
+
+pub struct TunWriter<T>
+where
+    T: AsyncWrite + Send + Sync,
+{
+    inner: WriteHalf<T>,
+    #[cfg(any(target_os = "macos", target_os = "freebsd"))]
+    buffer: Vec<u8>,
+}
+
+impl<T> TunWriter<T>
+where
+    T: AsyncWrite + Send + Sync,
+{
+    pub fn new(inner: WriteHalf<T>) -> Self {
+        Self {
+            inner,
+            #[cfg(any(target_os = "macos", target_os = "freebsd"))]
+            buffer: vec![0u8; 2038],
+        }
+    }
+}
+
+#[async_trait]
+impl<T> frame::RawWriter for TunWriter<T>
+where
+    T: AsyncWrite + Send + Sync,
+{
+    async fn write(&mut self, buf: &[u8]) -> tokio::io::Result<usize> {
+        #[cfg(any(target_os = "linux", target_os = "android"))]
+        {
+            self.inner.write_all(&buf).await?;
+            Ok(buf.len())
+        }
+        #[cfg(any(target_os = "macos", target_os = "freebsd"))]
+        {
+            let n = buf.len();
+            self.buffer[3] = 2;
+            self.buffer[4..4 + n].copy_from_slice(buf);
+            self.inner.write_all(&self.buffer[0..4 + n]).await?;
+            Ok(n)
+        }
+    }
+    async fn shutdown(&mut self) {
+        _ = self.inner.shutdown().await;
+    }
+}
+
+pub fn wrap_tun<T>(tun: T) -> (TunReader<T>, TunWriter<T>)
+where
+    T: AsyncRead + AsyncWrite + Sync + Send,
+{
+    let (tun_reader, tun_writer) = tokio::io::split(tun);
+    let tun_reader = TunReader::new(tun_reader);
+    let tun_writer = TunWriter::new(tun_writer);
+    (tun_reader, tun_writer)
+}
