1493cfb2e398b57355302a4f50caa3b63957b6f8
Centny <centny@gmail.com>
2023-11-14 13:39:23 +0800

    fix refresh should not build.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -94,6 +94,13 @@ class _MyHomePageState extends State<MyHomePage> {
   String _text = "";
   Timer? _ticker;
 
+  @override
+  void initState() {
+    super.initState();
+    _refreshState();
+    _startTick();
+  }
+
   void _startTick() {
     _ticker ??= Timer.periodic(const Duration(seconds: 2), _onTick);
   }
@@ -158,8 +165,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    _refreshState();
-    _startTick();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
